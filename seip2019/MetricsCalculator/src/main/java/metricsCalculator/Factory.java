5705e8a7b53b356126848d5bbf650b5018facbb5
Konstantinos Babetas <kbabetas@gmail.com>
2019-03-30 01:58:48 +0200

    Add Factory

---
+++ seip2019/MetricsCalculator/src/main/java/metricsCalculator/Factory.java
@@ -0,0 +1,23 @@
+package metricsCalculator;
+
+/**
+ * The purpose of this class is to play the role of the Factory in the design
+ * patter "Factory" and create an object of a Strategy.
+ * 
+ * @author Konstantinos Babetas
+ */
+
+public class Factory {
+
+	/**
+	 * The purpose of this method is to create the correct Code Analyzer
+	 * 
+	 * @param type is the type of Code Analyzer that will be created
+	 * @return strategy is the Code Analyzer
+	 */
+
+	public static CodeAnalyzerStrategy getCodeAnalyzerStrategy(String type) {
+		CodeAnalyzerStrategy strategy = CodeAnalyzerStrategy.CreateCodeAnalyzerStrategy(type);
+		return strategy;
+	}
+}
