b19b1bc16e254d154045003bea264a0e5cd4a45e
Konstantinos Babetas <kbabetas@gmail.com>
2019-04-21 14:32:25 +0300

    Test Add function

--- seip2019/unittesting/src/test/java/operations/IntegerOperationsTest.java
+++ seip2019/unittesting/src/test/java/operations/IntegerOperationsTest.java
@@ -23,5 +23,60 @@ public class IntegerOperationsTest {
 	public void test_reverseSign_zero() {
 		Assert.assertEquals(0, io.reverseSign(0));
 	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_negatives() {
+		io.add(-1, -5);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_negative_positive() {
+		io.add(-1, 5);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_positive_negative() {
+		io.add(1, -5);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_maxPositive_negative() {
+		io.add(Integer.MAX_VALUE, -5);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_maxPositives() {
+		io.add(Integer.MAX_VALUE, Integer.MAX_VALUE);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_maxPositive_positive() {
+		io.add(Integer.MAX_VALUE, 5);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_longPositive_positive() {
+		io.add(2147483647, 5);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_add_longPositives() {
+		io.add(2147483647, 2147483647);
+	}
+
+	@Test
+	public void test_add_positives() {
+		Assert.assertEquals(8, io.add(1, 5));
+	}
+	
+	@Test
+	public void test_add_zeroes() {
+		Assert.assertEquals(0, io.add(0, 0));
+	}
+	
+	@Test
+	public void test_add_positive_zero() {
+		Assert.assertEquals(1, io.add(1, 0));
+	}
 
 }
