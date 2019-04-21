d78a6ba3a48d826cd9b268e58b7c6e17cf0f6409
Konstantinos Babetas <kbabetas@gmail.com>
2019-04-21 15:04:23 +0300

    Add PowerOfTwo Parameterized test

---
+++ seip2019/unittesting/src/test/java/operations/IntegerOperationsTest_PowerOfTwo_Parameterized.java
@@ -0,0 +1,33 @@
+package operations;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class IntegerOperationsTest_PowerOfTwo_Parameterized {
+
+	@Parameter(value = 0)
+	public int power;
+	@Parameter(value = 1)
+	public int result;
+
+	IntegerOperations io = new IntegerOperations();
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][] { { 2, 4 }, { 3, 8 }, { 4, 16 } };
+		return Arrays.asList(data);
+	}
+
+	@Test
+	public void test_powerOfTwoNormalCases() {
+		Assert.assertEquals(result, io.powerOfTwo(power));
+	}
+
+}
