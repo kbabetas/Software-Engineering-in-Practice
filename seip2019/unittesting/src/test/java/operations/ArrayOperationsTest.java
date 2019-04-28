e7b6756b8e95699ab3595097fd4e033be8fa1988
Konstantinos Babetas <kbabetas@gmail.com>
2019-04-28 12:09:10 +0300

    Remove code duplication

--- seip2019/unittesting/src/test/java/operations/ArrayOperationsTest.java
+++ seip2019/unittesting/src/test/java/operations/ArrayOperationsTest.java
@@ -19,48 +19,36 @@ public class ArrayOperationsTest {
 	String invalid = "src/test/resources/invalid.txt";
 	String empty = "src/test/resources/empty.txt";
 	String dif_max = "src/test/resources/dif_max.txt";
+	FileIO fio = mock(FileIO.class);
+	IntegerOperations io = mock(IntegerOperations.class);
+	ArrayOperrations ao = new ArrayOperrations(fio, io);
 
 	@Test
 	public void test_findMax_valid() {
-		FileIO fio = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
-		ArrayOperrations ao = new ArrayOperrations(fio, io);
 		when(fio.readFile(valid)).thenReturn(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
 		Assert.assertEquals(15, ao.findMaxInFile(valid));
 	}
 
 	@Test
 	public void test_findMax_difMax() {
-		FileIO fio = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
-		ArrayOperrations ao = new ArrayOperrations(fio, io);
 		when(fio.readFile(dif_max)).thenReturn(new int[] { 10, 5, 12, 3, 15 });
 		Assert.assertEquals(15, ao.findMaxInFile(dif_max));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void test_findMax_empty() {
-		FileIO fio = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
-		ArrayOperrations ao = new ArrayOperrations(fio, io);
 		when(fio.readFile(empty)).thenReturn(new int[] {});
 		ao.findMaxInFile(empty);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void test_reverse_empty() {
-		FileIO fio = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
-		ArrayOperrations ao = new ArrayOperrations(fio, io);
 		when(fio.readFile(empty)).thenReturn(new int[] {});
 		ao.reverseArray(empty);
 	}
 
 	@Test
 	public void test_reverseArray_valid() {
-		FileIO fio = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
-		ArrayOperrations ao = new ArrayOperrations(fio, io);
 		when(fio.readFile(valid)).thenReturn(new int[] { 1, 2, 3, 4 });
 		when(io.reverseSign(1)).thenReturn(-1);
 		when(io.reverseSign(2)).thenReturn(-2);
